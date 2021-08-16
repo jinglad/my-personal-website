6ae0904629d85096d57e088528d6414b99bd131f
Jihan Chowdhury <57634584+jinglad@users.noreply.github.com>
2021-08-16 20:49:17 +0600

    update blog.js

--- src/Components/Blogs/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs/Blogs.js
@@ -6,6 +6,22 @@ const blogsData = [
     {
         title : "Some Html Interview Questions",
         link : "https://medium.com/@jihanchowdhury70/some-html-interview-questions-a97e48fbb02f",
+    },
+    {
+        title : "Some Javascript Coding Problems For Beginners",
+        link : "https://jihanchowdhury70.medium.com/some-javascript-coding-problems-for-beginners-dcfc5460ad6b"
+    },
+    {
+        title : "Things to know about React.JS",
+        link : "https://jihanchowdhury70.medium.com/things-to-know-about-react-js-893fedb4e2f8"
+    },
+    {
+        title : "Important Terms A Javascript Programmer Should Know",
+        link : "https://jihanchowdhury70.medium.com/important-terms-a-javascript-programmer-should-know-47ecfd31babe"
+    },
+    {
+        title : "Some Javascript Coding Problems For Beginners",
+        link : "https://jihanchowdhury70.medium.com/some-javascript-coding-problems-for-beginners-dcfc5460ad6b"
     }
 ]
 
